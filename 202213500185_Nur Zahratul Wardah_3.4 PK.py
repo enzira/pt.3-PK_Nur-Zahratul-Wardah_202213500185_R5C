034f89eb2abe5596624a86b61f0508b3bcb0c95d
enzira <nwardah24@gmail.com>
2024-10-04 09:14:04 +0700

    Add files via upload

---
+++ 202213500185_Nur Zahratul Wardah_3.4 PK.py
@@ -0,0 +1,25 @@
+#Get input from user
+sisi1=input("Masukkan sisi 1: ")
+sisi2=input("Masukkan sisi 2: ")
+sisi3=input("Masukkan sisi 3: ")
+
+#Try to convert inputs to intergers
+try:
+    sisi1=int(sisi1)
+    sisi2=int(sisi2)
+    sisi3=int(sisi3)
+except ValueError:
+    print("Eror: Input  harus dalam bilangan bulat.")
+    exit()
+
+#Check if all sides are equal
+if sisi1==sisi2==sisi3:
+    print("3 sisi sama")
+
+#Check if two sides are equal
+elif sisi1==sisi2 or sisi1==sisi3 or sisi2==sisi3:
+    print("2 sisi sama")
+
+#if none of the above conditions are true, print "Tidak ada yang sama"
+else:
+    print("Tidak ada yang sama")
